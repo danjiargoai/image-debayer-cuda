2ac77910390492fc05f847660b40fae833838760
Dan Ji <danji@argo.ai>
2017-04-25 17:51:27 -0700

    Switch to cudaEvent to get timing

--- HQDebayer.cpp
+++ HQDebayer.cpp
@@ -12,16 +12,13 @@ using namespace std;
 // Functions
 void cleanup(void);
 void initializeData(char *file) ;
-void computeTime(string str);
 
 // Global variables
 static int imWidth  = 0;   // Image width
 static int imHeight = 0;   // Image height
-StopWatchInterface *timer = NULL;
 
 int main(int argc, char **argv)
 {
-  sdkCreateTimer(&timer);
   // Helper
   if (checkCmdLineFlag(argc, (const char **)argv, "help"))
   {
@@ -29,55 +26,62 @@ int main(int argc, char **argv)
       exit(EXIT_SUCCESS);
   }
 
-  // Initialize data, copy it to device and set up texture memory
-  initializeData(argv[1]);
   outPixel* data = NULL;
-  checkCudaErrors(cudaMalloc((void**)&data, imWidth * imHeight * sizeof(outPixel)));
+  unsigned char* result = NULL;
+  cudaEvent_t start, stop;
+  float total_time = 0.0f;
+  float time;
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
 
-  if (data == NULL) {
-    cout << "Cannot allocate CUDA memory" << endl;
-    return 0; 
+  int i = 0;
+  while ( i < 10 ) {
+    // Initialize data, copy it to device and set up texture memory
+    initializeData(argv[1]);
+    if (data == NULL)
+      checkCudaErrors(cudaMalloc((void**)&data, imWidth * imHeight * sizeof(outPixel)));
+
+    if (result == NULL)
+      result = (unsigned char*)malloc(imWidth*imHeight*sizeof(outPixel));
+    
+    cudaEventRecord(start, 0);
+    // Execute Kernel
+    hqDebayer(data, imWidth, imHeight);
+    
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&time, start, stop);
+    cout << "Kernel execution time " << time << endl;
+    total_time += time;
+    cudaEventRecord(start, 0);
+    // Copy data from device to host 
+    checkCudaErrors(cudaMemcpy(result, data, imWidth*imHeight*sizeof(outPixel), cudaMemcpyDeviceToHost));
+    
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&time, start, stop);
+    cout << "Device to Host time " << time << endl;
+    cout << endl;
+    i++;
   }
 
-  // Timer
-  sdkResetTimer(&timer);
-  sdkStartTimer(&timer);
-  
-  hqDebayer(data, imWidth, imHeight);
-  checkCudaErrors(cudaDeviceSynchronize());
-  
-  sdkStopTimer(&timer);
-  computeTime("Kernel Execution time");
- 
-  // Copy data from device to host 
-  unsigned char* result = (unsigned char*)malloc(imWidth*imHeight*sizeof(outPixel));
-  // Timer
-  sdkResetTimer(&timer);
-  sdkStartTimer(&timer);
-  
-  checkCudaErrors(cudaMemcpy(result, data, imWidth*imHeight*sizeof(outPixel), cudaMemcpyDeviceToHost));
-  
-  sdkStopTimer(&timer);
-  computeTime("Device to Host time");
-  
+  cout << "Average Kernel execution time is " << total_time / 10 << endl;
   Mat image_out(imHeight, imWidth, CV_8UC3, result);
   imwrite("out.jpg", image_out);
+  imshow("Debayered", image_out);
+  waitKey(0);
 
   // Free up memeory usage
   checkCudaErrors(cudaFree(data));
   free(result);
   cleanup();
-}
-
-void computeTime(string str)
-{
-    cout << str << " " << sdkGetTimerValue(&timer) << " ms "<< endl;
+  cudaEventDestroy(start);
+  cudaEventDestroy(stop);
 }
 
 void cleanup(void)
 {
     deleteTexture();
-    sdkDeleteTimer(&timer);
 }
 
 void initializeData(char *file)
@@ -90,7 +94,7 @@ void initializeData(char *file)
   imHeight = image.rows;
   g_Bpp = image.channels();
   pixels = image.data;
-  setupTexture(imWidth, imHeight, pixels, g_Bpp, timer);
+  setupTexture(imWidth, imHeight, pixels, g_Bpp);
 
   memset(pixels, 0x0, g_Bpp * sizeof(Pixel) * imWidth * imHeight);
 }
